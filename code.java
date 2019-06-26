3a9f92e36efc17e3c9f55bde23fd1aed3d6b047c
sandeepchahal1997 <sandeep.chahal@sofbang.com>
2019-06-26 15:56:54 +0530

    new code is added

---
+++ code.java
@@ -0,0 +1,12 @@
+class code{
+ 	    public static void main(String[] args)
+                 {
+	        	System.out.println("Good Evening Sumit");
+			System.out.println("How was your day");
+			System.out.println("We will contact you later Aplha.fiit");
+			int i;
+			for(i=0;i<5;i++){
+				System.out.println("We will contact you later Aplha.fiit"+i);
+			}
+                 }
+	  }
